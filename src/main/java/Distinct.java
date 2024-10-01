f054178201652ff8d325d5a4324b16ec2e97c703
Michal Sroka <michal.sroka.work@gmail.com>
2024-10-01 20:04:03 +0200

    Distinct

---
+++ src/main/java/Distinct.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Distinct {
+    public int solution(int[] A) {
+        if (A.length == 0) {
+            return 0;
+        }
+        if (A.length == 1) {
+            return 1;
+        }
+
+        Arrays.sort(A);
+
+        int distinctValues = 1;
+
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] != A[i - 1]) {
+                distinctValues++;
+            }
+        }
+
+        return distinctValues;
+    }
+}
