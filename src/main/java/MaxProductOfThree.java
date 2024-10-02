9b9eb0fa276397a816837cfefcdfd2238b656d44
Michal Sroka <michal.sroka.work@gmail.com>
2024-10-02 12:01:14 +0200

    MaxProductOfThree - refactored

--- src/main/java/MaxProductOfThree.java
+++ src/main/java/MaxProductOfThree.java
@@ -10,30 +10,11 @@ public class MaxProductOfThree {
                 "Array length must be between 3 and 100_000"
         );
 
-        if (A.length == 3) {
-            return A[0] * A[1] * A[2];
-        }
-
         Arrays.sort(A);
 
-        int firstItem = A[0];
-        int secondItem = A[1];
-
-        int thirdFromLastItem = A[A.length - 3];
-        int secondFromLastItem = A[A.length - 2];
-        int lastItem = A[A.length - 1];
-
-        if (firstItem >= 0 || lastItem <= 0) {
-            return thirdFromLastItem * secondFromLastItem * lastItem;
-        }
-
-        if (secondFromLastItem < 0 || thirdFromLastItem < 0) {
-            return firstItem * secondItem * lastItem;
-        }
-
         return Math.max(
-                thirdFromLastItem * secondFromLastItem * lastItem,
-                firstItem * secondItem * lastItem
+                A[0] * A[1] * A[A.length - 1],
+                A[A.length - 3] * A[A.length - 2] * A[A.length - 1]
         );
     }
 }
