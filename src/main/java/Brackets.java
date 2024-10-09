200c6ee7ee29709ab0f4b72a98c83146483f8c1b
Michal Sroka <michal.sroka.work@gmail.com>
2024-10-09 18:49:27 +0200

    Brackets - preconditions added

--- src/main/java/Brackets.java
+++ src/main/java/Brackets.java
@@ -1,8 +1,12 @@
 import java.util.Stack;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 public class Brackets {
 
     public int solution(String S) {
+
+        preconditions(S);
 
         Stack<Character> openingBrackets = new Stack<>();
         Stack<Character> closingBrackets = new Stack<>();
@@ -32,5 +36,23 @@ public class Brackets {
             case '(' -> ')';
             default -> throw new IllegalArgumentException("Invalid bracket: " + bracket);
         };
+    }
+
+    private void preconditions(String S) {
+        checkArgument(
+                S.length() <= 200_000,
+                "String length must be less or equal than 200_000"
+        );
+
+        for (char currentChar : S.toCharArray()) {
+            checkArgument(currentChar == '{'
+                            || currentChar == '}'
+                            || currentChar == '['
+                            || currentChar == ']'
+                            || currentChar == '('
+                            || currentChar == ')',
+                    "Each char must be a valid bracket"
+            );
+        }
     }
 }
